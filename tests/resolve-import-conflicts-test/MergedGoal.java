98e0046d1d9c3e8cf83cce3d4c0b0e920217be69
Michael Ernst <mernst@cs.washington.edu>
2023-09-27 15:45:50 -0700

    Update tests

--- tests/resolve-import-conflicts-test/MergedGoal.java
+++ tests/resolve-import-conflicts-test/MergedGoal.java
@@ -8,9 +8,13 @@ import x.e3;
 import x.f;
 import x.g;
 import x.h;
+import x.h;
+import x.h;
+import x.h;
+import x.h;
 import x.i;
-import x.k;
 import x.j;
+import x.k;
 import x.l;
 
 class C {}
