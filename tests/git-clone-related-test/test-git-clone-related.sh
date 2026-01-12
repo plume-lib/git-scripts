337761a602935c1ecdf05352805d24eff0188d4e
Michael Ernst <mernst@cs.washington.edu>
2026-01-12 07:48:56 -0800

    Add diagnostics

--- tests/git-clone-related-test/test-git-clone-related.sh
+++ tests/git-clone-related-test/test-git-clone-related.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
-# Test one invocation of git-clone-related
+# Test one invocation of git-clone-related.
 
-# arguments:
+# Arguments:
 #  1: repo from which to run git-clone-related
 #  2: branch from which to run git-clone-related
 #  3: git-clone-related arguments
-#  4: repo that should be cloned
-#  5: branch that should be cloned
+#  4: expected repo that should be cloned
+#  5: expected branch that should be cloned
 
 START_REPO=$1
 START_BRANCH=$2
@@ -17,15 +17,17 @@ GOAL_BRANCH=$5
 
 set -o errexit -o nounset
 # set -o pipefail
+
 # Display commands and their arguments as they are executed.
 # set -x
-# set -v : Display shell input lines as they are read.
+# Display shell input lines as they are read.
+# set -v
 
 USER=${USER:-git-clone-related}
-PLUME_SCRIPTS=$(cd ../../ && pwd -P)
-startdir=$(mktemp -d 2> /dev/null || mktemp -d -t 'startdir')
-goaldir=$(mktemp -d 2> /dev/null || mktemp -d -t 'goaldir')
-rm -rf "$startdir" "$goaldir"
+GIT_SCRIPTS=$(cd ../../ && pwd -P)
+startdir=$(mktemp -d --tmpdir test-git-clone-related-startdir-XXXXXX 2> /dev/null || mktemp -d 2> /dev/null)
+resultdir=$(mktemp -d --tmpdir test-git-clone-related-resultdir-XXXXXX 2> /dev/null || mktemp -d 2> /dev/null)
+rm -rf "$startdir" "$resultdir"
 
 git clone --branch "$START_BRANCH" "$START_REPO" "$startdir" -q --single-branch --depth 1
 # This test might itself be running under CI, so unset the variables that
@@ -35,21 +37,26 @@ unset BUILD_SOURCEBRANCH
 unset TRAVIS
 unset CIRCLE_COMPARE_URL
 unset GITHUB_HEAD_REF
-# shellcheck disable=SC2086  # $ARGS should not be quoted
-(cd "$startdir" && "${PLUME_SCRIPTS}"/git-clone-related $ARGS "$goaldir")
-clonedrepo=$(git -C "$goaldir" config --get remote.origin.url)
-# git 2.22 and later has `git branch --show-current`; CircleCI doesn't have that version yet.
-clonedbranch=$(git -C "$goaldir" rev-parse --abbrev-ref HEAD)
 
-rm -rf "$startdir" "$goaldir"
+echo "$0: About to run: (cd $startdir && ${GIT_SCRIPTS}/git-clone-related $ARGS $resultdir)"
+# shellcheck disable=SC2086  # $ARGS should not be quoted
+(cd "$startdir" && "${GIT_SCRIPTS}"/git-clone-related $ARGS "$resultdir")
+echo "$0: Done: (cd $startdir && ${GIT_SCRIPTS}/git-clone-related $ARGS $resultdir)"
+
+clonedrepo=$(git -C "$resultdir" config --get remote.origin.url)
+# git 2.22 and later has `git branch --show-current`; CircleCI doesn't have that version yet.
+clonedbranch=$(git -C "$resultdir" rev-parse --abbrev-ref HEAD)
 
 if [ "$clonedrepo" != "$GOAL_REPO" ]; then
   echo "test-git-clone-related.sh \"$1\" \"$2\" \"$3\" \"$4\" \"$5\""
-  echo "expected repo $GOAL_REPO, got: $clonedrepo"
+  echo "expected repo $GOAL_REPO, got $clonedrepo, in $resultdir"
   exit 2
 fi
 if [ "$clonedbranch" != "$GOAL_BRANCH" ]; then
   echo "test-git-clone-related.sh \"$1\" \"$2\" \"$3\" \"$4\" \"$5\""
-  echo "expected branch $GOAL_BRANCH, got: $clonedbranch"
+  echo "expected branch $GOAL_BRANCH, got $clonedbranch, in $resultdir"
   exit 2
 fi
+
+# This comes after the failure exits, so we don't clean up if the test fails.
+rm -rf "$startdir" "$resultdir"
